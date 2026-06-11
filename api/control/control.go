ce5c0e6dc18a9da5ae4d5c90c66b58261fb2fd81
Henri Lepic <henrilepic@gmail.com>
2026-06-11 11:29:57 +0200

    inserst products.

--- api/control/control.go
+++ api/control/control.go
@@ -20,17 +20,5 @@ func (c *Control) Register(u *model.User) error {
 }
 
 func (c *Control) GetProducts() ([]model.Product, error) {
-
 	return c.db.GetProducts()
-
-	// data, err := os.ReadFile("../../front/public/products.json")
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to read products data: %w", err)
-	// }
-	// var products []model.Product
-	// err = json.Unmarshal(data, &products)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to parse products data: %w", err)
-	// }
-	// return products, nil
 }
--- api/db/sqlite/sqlite.go
+++ api/db/sqlite/sqlite.go
@@ -2,6 +2,9 @@ package sqlite
 
 import (
 	"apisanta/model"
+	"encoding/json"
+	"log"
+	"os"
 
 	"github.com/libtnb/sqlite"
 	"gorm.io/gorm"
@@ -22,12 +25,36 @@ func New() (*SQLiteDB, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	err = db.AutoMigrate(&model.User{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &SQLiteDB{db: db}, nil
+	dbconn := &SQLiteDB{db: db}
+	ps, _ := dbconn.GetProducts()
+	if len(ps) == 0 {
+		insertSampleData(db)
+	}
+
+	return dbconn, nil
+}
+
+func insertSampleData(db *gorm.DB) {
+	data, err := os.ReadFile("../front/public/products.json")
+	if err != nil {
+		log.Printf("failed to read products data: %v", err)
+		return
+	}
+	var products []model.Product
+	err = json.Unmarshal(data, &products)
+	if err != nil {
+		log.Printf("failed to parse products data: %v", err)
+		return
+	}
+	for _, product := range products {
+		db.Create(&product)
+	}
 }
 
 func (s *SQLiteDB) GetProducts() ([]model.Product, error) {
