f24ac8211feb43d73da6859ef7dee7c71aa70ee5
Henri Lepic <henrilepic@gmail.com>
2026-06-11 09:51:49 +0200

    add control to handler init

--- api/main.go
+++ api/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
+	"apisanta/control"
 	"apisanta/handler"
 )
 
 func main() {
 	router := gin.Default()
 
-	handler.Init(router)
+	control := control.New()
+
+	handler.Init(router, control)
 
 	router.Run(":8080")
 }
